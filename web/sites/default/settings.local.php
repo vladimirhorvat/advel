fefdbf78e1ad677bcc0e4714f30b60e962f4982d
Vladimir Horvat <vladimir.horvat@fermicoding.com>
2020-07-24 22:17:11 +0200

    settings

---
+++ web/sites/default/settings.local.php
@@ -0,0 +1,16 @@
+<?php
+$config_directories['sync'] = '../config/sync';
+$databases['default']['default'] = array(
+  'database' => 'advel',
+  'username' => '',
+  'password' => '',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['trusted_host_patterns'] = array('^advel$');
+$settings['file_private_path'] = '/home/fc/1/rs/advel/private';
+$config_directories['sync'] = '../config/sync';
+$settings['config_sync_directory'] = '../config/sync';
